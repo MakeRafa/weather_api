894c9ab724cd8701e98b8d34670f9bc9c52beeef
MakeRafa <rafavazquez229@gmail.com>
2021-06-18 12:53:54 -0700

    routes finished, css next

--- app.py
+++ app.py
@@ -18,12 +18,31 @@ print = PrettyPrinter(indent=5)
 API_KEY=os.getenv('API_KEY')
 API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
-def home():
-    return render_template('home.html')
 
+@app.route('/')
+def home():
+    return render_template('form.html')
+
+@app.route('/results')
 def current_weather():
-    
-    return
+    city = request.args.get('city')
+    mood = request.args.get('mood')
+
+    params = {
+        'appid': API_KEY,
+
+        'q': city
+    }
+
+    result_json = requests.get(API_URL, params=params).json()
+    print.pprint(result_json)
+
+    context = {
+        'mood': mood,
+        'city': city,
+        'description': result_json['weather'][0]['main']
+    }
+    return render_template('results.html', **context)
 
 
 
